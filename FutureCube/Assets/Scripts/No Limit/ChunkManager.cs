11f8ed47fd5880d65fb75be4f66db2ace4780146
Solomon Rosemite™ <60587271+SolomonRosemite@users.noreply.github.com>
2020-08-25 14:57:26 +0200

    Chunks now get Unloaded

--- FutureCube/Assets/Scripts/No Limit/ChunkManager.cs
+++ FutureCube/Assets/Scripts/No Limit/ChunkManager.cs
@@ -9,21 +9,27 @@ public class ChunkManager : MonoBehaviour
     public UnityEngine.Object prefab;
     public int PreviousId { get; private set; } = -1;
 
+    private Queue<GameObject> queueOfChunks = new Queue<GameObject>();
+
     void Start() => ins = this;
 
     public void OnEnterNewChunk(GameObject go, Chunk chunk)
     {
         if (PreviousId == chunk.ChunkId) { return; }
+
         var position = go.transform.position;
         float z = position.z + go.transform.localScale.z;
 
         PreviousId = chunk.ChunkId;
-        print(PreviousId);
+
+        queueOfChunks.Enqueue(go);
 
         CreateChunk(
             new Chunk(chunk.ChunkId + 1, chunk.Difficulty),
             new Vector3(position.x, position.y - 0.03f, z)
         );
+
+        UnloadChunk();
     }
 
     private void CreateChunk(Chunk values, Vector3 position)
@@ -32,5 +38,10 @@ public class ChunkManager : MonoBehaviour
         ChunkHolder chunkHolder = chunk.GetComponent<ChunkHolder>();
 
         chunkHolder.updateChunkValues(values);
+    }
+
+    private void UnloadChunk()
+    {
+        if (queueOfChunks.Count > 2) { Destroy(queueOfChunks.Dequeue()); }
     }
 }
