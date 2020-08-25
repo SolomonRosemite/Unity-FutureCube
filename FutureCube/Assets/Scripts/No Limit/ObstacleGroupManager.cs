b1adec6c8caeece49379f53be3baa7152596b829
Solomon Rosemite™ <60587271+SolomonRosemite@users.noreply.github.com>
2020-08-25 22:13:07 +0200

    Now works also with the Green Zone

--- FutureCube/Assets/Scripts/No Limit/ObstacleGroupManager.cs
+++ FutureCube/Assets/Scripts/No Limit/ObstacleGroupManager.cs
@@ -17,12 +17,13 @@ public class ObstacleGroupManager : MonoBehaviour
         int zScale = new System.Random().Next(10, 35);
 
         Vector3 greenZoneVector = greenZone.localScale;
-        Vector3 whiteZoneVector = whiteZone.localScale;
-
         greenZoneVector.z = zScale;
-        whiteZoneVector.z = length - blueZone.localScale.z - zScale;
 
         greenZone.localScale = greenZoneVector;
+
+        Vector3 whiteZoneVector = whiteZone.localScale;
+        whiteZoneVector.z = length - blueZone.localScale.z - zScale;
+
         whiteZone.localScale = whiteZoneVector;
 
         SetPostions();
@@ -36,20 +37,16 @@ public class ObstacleGroupManager : MonoBehaviour
 
         whiteZonePosition.z -= whiteZone.localScale.z / 2;
         whiteZonePosition.z -= 10;
+
         whiteZone.position = whiteZonePosition;
-        // whiteZonePosition.z = blueZone.position.z;
 
-        // whiteZonePosition.z -= whiteZone.localScale.z / 2 - 30;
-        // whiteZone.position = whiteZonePosition;
+        Vector3 greenZonePosition = greenZone.position;
 
-        // // Vector3 greenZonePosition = greenZone.position;
+        greenZonePosition.z = blueZone.position.z;
 
-        // // print(blueZone.localScale.z);
-        // // print(whiteZone.localScale.z);
-        // // print(blueZone.localScale.z / 2 + whiteZone.localScale.z / 2);
+        greenZonePosition.z -= greenZone.localScale.z / 2;
+        greenZonePosition.z -= 10 + whiteZone.localScale.z;
 
-        // whiteZonePosition.z = 150 - blueZone.localScale.z + whiteZone.localScale.z;
-
-        // // whiteZone.position = whiteZonePosition;
+        greenZone.position = greenZonePosition;
     }
 }
